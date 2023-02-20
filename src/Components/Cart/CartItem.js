5318c653b5d36a86b240bd44d27c06a0f583753f
Yelena <88336329+DYA13@users.noreply.github.com>
2023-02-20 18:33:04 +0100

    CartItem edited

--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -10,7 +10,7 @@ const CartItem = ({cartItem}) => {
 
     return (<div>
     <p>{oggetti.name}</p>
-    <p> {cartItem.quantity} portion(s)</p>
+    <p> {cartItem.quantity} item(s)</p>
     <p>Price: ${oggetti.price * cartItem.quantity}</p>
     <span onClick={() => dispatch(removeItemFromCart({cartItemId: cartItem.id}))}>  
     <img className="icon" src="https://img.icons8.com/material-outlined/48/000000/trash--v1.png" alt="deleteIcon"/>
