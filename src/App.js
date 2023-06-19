840cda0768f73d4d2f76919ddd1c7a15f8f2729f
Yelena Japarova <yelena.dzhaparova@gmail.com>
2023-06-19 22:49:30 +0200

    modified:   src/App.js,modified:   src/data/HeaderContent.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import "./App.css";
-import Oggetti from "./Components/OggettiComponents/Oggetti";
-import AllCategories from "./Components/Filter/AllCategories";
-import Cart from "./Components/Cart/Cart";
-import Header from "./data/HeaderContent";
+import './App.css'
+import Oggetti from './Components/OggettiComponents/Oggetti'
+import AllCategories from './Components/Filter/AllCategories'
+import Cart from './Components/Cart/Cart'
+import Header from './data/HeaderContent'
 
 function App() {
   return (
-    <div >
+    <div>
       <header>
-      <nav className="header_content">  
-   
-      <Header/>
-
+        <nav className='header_content'>
+          <Header />
         </nav>
       </header>
 
-        <div className="aside">
+      <div className='aside'>
         <AllCategories />
         <Cart />
-        </div>
-        
-        <section>
-        <div className="oggetti_container">  
-        <Oggetti />
-        </div>
-        </section>
-     
-        </div>
+      </div>
 
-
-  );
+      <section>
+        <div className='oggetti_container'>
+          <Oggetti />
+        </div>
+      </section>
+    </div>
+  )
 }
 
-export default App;
+export default App
